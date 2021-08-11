bd97de980aeeba20f75abad1ad72ce9de6050fa2
sourav <SD056640@cerner.net>
2021-08-11 16:02:27 +0530

    Updated tests

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+public class TestClass {
+  @Test
+  public void testNextState() {
+    Assert.assertEquals("1", "1", "1");
+  }
+  @Test
+  public void testChange() {
+	Assert.assertEquals("2", "2", "2");
+  }
+}
